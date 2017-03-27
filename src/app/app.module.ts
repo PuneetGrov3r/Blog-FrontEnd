3032b05008e74c60ddcda33fd8cd831dae83e468
Puneet Grover <grover.puneet1995@gmail.com>
2017-03-27 18:03:01 +0530

    0.0.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule }      from '@angular/core';
+import { NgModule, AfterContentInit }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import $ from 'jquery';
+
 
 import { AppComponent }  from './app.component';
 import { Home } from './components/home/home.component';
@@ -11,4 +13,59 @@ import { Footer } from './shared/footer/footer';
   declarations: [ AppComponent, Home, Header, Footer],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule implements AfterContentInit {
+  ngAfterContentInit() {
+$(document).ready(function () {
+   let $menumain = document.getElementsByClassName('menumain');
+    let $signin = document.getElementsByClassName('signin');
+    let $signup = document.getElementsByClassName('signup');
+    let $about = document.getElementsByClassName('aboutshow');
+    let country = [];
+
+    function sidemenutoggle() {
+        $('.ui.sidebar')
+            .sidebar('toggle');
+    }
+
+    function signinshow() {
+        $('.ui.signin1.modal')
+            .modal('show');
+    }
+
+    function countrylist() {
+        $('.ui.search')
+            .search({
+                source: country
+            });
+    }
+
+    function signupshow() {
+        $('.ui.signup1.modal')
+            .modal('show');
+    }
+
+    function aboutshow() {
+        $('.ui.small.modal')
+            .modal('show');
+    }
+    for (let i = 0; i < $signup.length; i++) {
+        $signup[i].addEventListener('click', signupshow, false);
+    }
+    for (let i = 0; i < $signup.length; i++) {
+        $signup[i].addEventListener('click', signupshow, false);
+    }
+    for (let i = 0; i < $about.length; i++) {
+        $about[i].addEventListener('click', aboutshow, false);
+    }
+    for (let i = 0; i < $menumain.length; i++) {
+        $menumain[i].addEventListener('click', sidemenutoggle, false);
+    }
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade');
+        });
+}
+  },
+ }
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -2,44 +2,49 @@
  * System configuration for Angular samples
  * Adjust as necessary for your application needs.
  */
-(function (global) {
-  System.config({
-    paths: {
-      // paths serve as alias
-      'npm:': 'node_modules/'
-    },
-    // map tells the System loader where to look for things
-    map: {
-      // our app is within the app folder
-      'app': 'app',
+(function(global) {
+    System.config({
+        paths: {
+            // paths serve as alias
+            'npm:': 'node_modules/',
 
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+        },
+        // map tells the System loader where to look for things
+        map: {
+            // our app is within the app folder
+            'app': 'app',
+            'jquery': 'https://code.jquery.com/jquery-3.1.1.min.js',
+            'semantic_ui_sidebar': 'npm:semantic-ui-sidebar/sidebar.min.js',
+            'semantic_ui_dimmer': 'npm:semantic-ui-dimmer/dimmer.min.js',
+            'semantic_ui_modal': 'npm:semantic-ui-modal/modal.min.js',
+            'semantic_ui_transition': 'npm:semantic-ui-transition/transition.min.js',
+            // angular bundles
+            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+            '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
-      // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
-    },
-    // packages tells the System loader how to load when no filename and/or no extension
-    packages: {
-      app: {
-        defaultExtension: 'js',
-        meta: {
-          './*.js': {
-            loader: 'systemjs-angular-loader.js'
-          }
+            // other libraries
+            'rxjs': 'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+        },
+        // packages tells the System loader how to load when no filename and/or no extension
+        packages: {
+            app: {
+                defaultExtension: 'js',
+                meta: {
+                    './*.js': {
+                        loader: 'systemjs-angular-loader.js'
+                    }
+                }
+            },
+            rxjs: {
+                defaultExtension: 'js'
+            }
         }
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      }
-    }
-  });
+    });
 })(this);
